3210436c16312dbfd05db6388ab4464e4d156979
Steven Wilson Kidd <kidd9@data.cs.purdue.edu>
2017-04-17 17:57:41 -0400

    Commit

--- src/server/Request.cpp
+++ src/server/Request.cpp
@@ -15,7 +15,7 @@
 #include "error/RequestError.hpp"
 #include "error/ConnectionError.hpp"
 #include "error/TodoError.hpp"
-//#include "../parse/lex.yy.c"
+#include "../parse/lex.yy.c"
 #include "../parse/y.tab.c"
 
 Request::Request(Config const& config, TcpConnection& conn) :
