1c2983d685f1df73acf1a45d9e976538b7bf3040
Steven Wilson Kidd <kidd9@data.cs.purdue.edu>
2017-04-18 12:09:16 -0400

    Commit

--- src/server/Request.cpp
+++ src/server/Request.cpp
@@ -18,7 +18,7 @@
 #include "../parse/lex.yy.c"
 #include "../parse/y.tab.c"
 
-Request(Config const& config, const TcpConnection *conn)
+Request(Config)// const& config, const TcpConnection *conn)
 {
     m_config = config;
     m_conn = conn;
