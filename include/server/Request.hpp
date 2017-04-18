9b2f0d201e72ef77f552c487f0a90dbe8d1d57b8
Steven Wilson Kidd <kidd9@data.cs.purdue.edu>
2017-04-18 12:20:41 -0400

    Commit

--- include/server/Request.hpp
+++ include/server/Request.hpp
@@ -42,7 +42,7 @@ public:
     std::string m_method;
     std::string m_version;
 private:
-    Config const& m_config;
+    Config m_config;
     const TcpConnection *m_conn;
     std::unordered_map<std::string, std::string> m_headers;
     std::unordered_map<std::string, std::string> m_query;
