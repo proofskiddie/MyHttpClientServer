acd859990571ea5337fff3a2359ec1cc7114e2d2
Steven Wilson Kidd <kidd9@data.cs.purdue.edu>
2017-04-18 12:38:54 -0400

    Commit

--- src/server/Server.cpp
+++ src/server/Server.cpp
@@ -81,7 +81,7 @@ void Server::handle(TcpConnection* conn) //const /*hope this dosent break the th
     try
     {
         // creating res as an empty response
-        Response res(&m_config, conn);
+        Response res(m_config, *conn);
 
         // Printing the request will be helpful to tell what our server is seeing
         _currentRequest->print();
