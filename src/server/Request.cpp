10a338cf700235b0f188c9152c11535b4cd16f45
Steven Wilson Kidd <kidd9@data.cs.purdue.edu>
2017-04-18 14:27:58 -0400

    Commit

--- src/server/Request.cpp
+++ src/server/Request.cpp
@@ -15,13 +15,17 @@
 #include "error/RequestError.hpp"
 #include "error/ConnectionError.hpp"
 #include "error/TodoError.hpp"
-#include "../parse/y.tab.h"
+#include "../parse/y.tab.c"
+
 Request::Request(Config *config, TcpConnection *conn)
-{
+{ 
     m_config = config;
     m_conn = conn;
-    
     std::string request_line = parse_raw_line();
+    yy_scan_string(request_line.c_str());
+    yylex();
+    yyparse();
+    yy_delete_buffer(YY_CURRENT_BUFFER);
     //parse_method(request_line);
     //parse_route(request_line);
     //parse_version(request_line);
