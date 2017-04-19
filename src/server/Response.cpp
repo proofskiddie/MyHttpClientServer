9760dff1763b69aeb7a86fa8666a6cdd190c2907
Steven Wilson Kidd <kidd9@data.cs.purdue.edu>
2017-04-19 17:12:56 -0400

    Commit

--- src/server/Response.cpp
+++ src/server/Response.cpp
@@ -21,7 +21,7 @@ Response::Response(Config const& config, TcpConnection& conn) :
 
 void Response::send(void const* buf, size_t bufsize, bool raw)
 {
-    m_conn.puts(m_status_text + "\r\n");
+    m_conn.puts("HTTP/1.0" + m_status_text + "\r\n");
     if(!raw)
     	send_headers();
     m_conn.puts("\r\n");
