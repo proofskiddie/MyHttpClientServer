189cf8a9f8e9a7077dfec5d88d5266a84e827610
Steven Wilson Kidd <kidd9@data.cs.purdue.edu>
2017-04-18 23:19:40 -0400

    Commit

--- src/server/Request.cpp
+++ src/server/Request.cpp
@@ -23,12 +23,21 @@ Request::Request(Config *config, TcpConnection *conn)
     m_config = config;
     m_conn = conn;
     std::string request_line;
+    yyscan_t scanner;
+    YY_BUFFER_STATE buf;
+    yylex_init(&scanner);
     while (request_line = parse_raw_line(), request_line.compare("\r\n")) {
-    	yy_scan_string(request_line.c_str());
+	buf = yy_scan_string(request_line, scanner);
+	yylex(scanner);
+	/*
+	yy_scan_string(request_line.c_str());
     	yylex();
     	yyparse();
     	yy_delete_buffer(YY_CURRENT_BUFFER);
+    	*/
     }
+    yy_delete_buffer(buf, scanner);
+    yylex_destroy(scanner);
     //parse_method(request_line);
     //parse_route(request_line);
     //parse_version(request_line);
