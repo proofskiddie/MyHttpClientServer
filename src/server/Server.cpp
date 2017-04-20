9667130f391305d6bbadb330fd72061a4cf1c248
Steven Wilson Kidd <kidd9@data.cs.purdue.edu>
2017-04-19 21:07:46 -0400

    Commit

--- src/server/Server.cpp
+++ src/server/Server.cpp
@@ -58,18 +58,6 @@ void Server::run_linear()
 
 void Server::run_thread_request() //const
 {
-    int num_threads = m_config->threads;
-    std::thread threads[num_threads];
-    for (int i = 0; i < num_threads; ++i) {
-        TcpConnection *conn = new TcpConnection(*m_config, m_master);
-    	threads[i] = std::thread( [this, conn] () -> void {
-	   	this->handle(conn);
-		delete conn;
-	    });
-    } 
-    for (int i = 0; i < num_threads; ++i)
-    	threads[i].join();
-
 }
 
 void Server::run_fork() //const
@@ -89,7 +77,18 @@ void Server::run_fork() //const
 
 void Server::run_thread_pool() //const
 {
-    throw TodoError("3", "You need to implement thread-pool mode");
+    int num_threads = m_config->threads;
+    std::thread threads[num_threads];
+    for (int i = 0; i < num_threads; ++i) {
+        TcpConnection *conn = new TcpConnection(*m_config, m_master);
+    	threads[i] = std::thread( [this, conn] () -> void {
+	   	this->handle(conn);
+		delete conn;
+	    });
+    } 
+    for (int i = 0; i < num_threads; ++i)
+    	threads[i].join();
+
 }
 
 void Server::handle(TcpConnection* conn) //const
