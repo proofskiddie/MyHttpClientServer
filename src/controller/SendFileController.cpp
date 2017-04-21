bfd8b688b575443722644ee79f94d49f1bed43c0
Steven Wilson Kidd <kidd9@data.cs.purdue.edu>
2017-04-21 18:24:05 -0400

    Commit

--- src/controller/SendFileController.cpp
+++ src/controller/SendFileController.cpp
@@ -26,13 +26,11 @@ SendFileController::SendFileController(Config const& config, TcpConnection& conn
 
 void SendFileController::run(Request const& req, Response& res) const
 {	
-	if (req.get_path().empty())
-		return;
 	std::string path;
 	if (resolve_requested_path(req.get_path(), m_config.static_dir, path)) {
 		std::fstream fs(path);
 		if (!fs)
-			send_error_response(m_config, &m_conn, HttpStatus::NotFound, path + " could not be found");
+			send_error_response(m_config, &m_conn, HttpStatus::NotFound, req.get_path() + " could not be found");
 		int length = get_content_length(fs);
 		char *buf = new char[length];
     		res.set_status(HttpStatus::Ok);
@@ -41,7 +39,7 @@ void SendFileController::run(Request const& req, Response& res) const
 		fs.read(buf, length);
 		res.send(buf, length);
 	} else
-		send_error_response(m_config, &m_conn, HttpStatus::NotFound, path + " could not be found");
+		send_error_response(m_config, &m_conn, HttpStatus::NotFound, req.get_path() + " could not be found");
 }
 
 int SendFileController::get_content_length(std::fstream& fs) const
