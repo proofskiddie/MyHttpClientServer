50bdfd8059fe898ffbb18eeb8212d04d5ec57d37
Steven Wilson Kidd <kidd9@data.cs.purdue.edu>
2017-04-21 18:55:34 -0400

    Commit

--- src/controller/SendFileController.cpp
+++ src/controller/SendFileController.cpp
@@ -27,7 +27,7 @@ SendFileController::SendFileController(Config const& config, TcpConnection& conn
 void SendFileController::run(Request const& req, Response& res) const
 {	
 	std::string path;
-	if (req.get_path().empty()) return;
+	//if (req.get_path().empty()) return;
 	if (resolve_requested_path(req.get_path(), m_config.static_dir, path)) {
 		std::fstream fs(path);
 		if (!fs)
