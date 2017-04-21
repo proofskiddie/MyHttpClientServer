ce0b14abb1efcd27b933cb41b1c31117b1f394a8
Steven Wilson Kidd <kidd9@data.cs.purdue.edu>
2017-04-20 23:28:03 -0400

    Commit

--- src/controller/SendFileController.cpp
+++ src/controller/SendFileController.cpp
@@ -29,7 +29,7 @@ void SendFileController::run(Request const& req, Response& res) const
 	if (resolve_requested_path(req.get_path(), m_config.static_dir, path)) {
 		std::fstream fs(path);
 		if (!fs)
-			send_error_response(m_config, res.m_conn, HttpStatus::NotFound, path + " could not be found");
+			send_error_response(m_config, &res.m_conn, HttpStatus::NotFound, path + " could not be found");
 		int length = get_content_length(fs);
 		char *buf = new char[length];
     		res.set_status(HttpStatus::Ok);
