5a4cdc7a27a108a14eee7933f447655cb40c095a
Steven Wilson Kidd <kidd9@data.cs.purdue.edu>
2017-04-21 23:12:30 -0400

    Commit

--- src/controller/ExecScriptController.cpp
+++ src/controller/ExecScriptController.cpp
@@ -31,12 +31,14 @@ void ExecScriptController::run(Request const& req, Response& res) const
 
 bool ExecScriptController::set_environment(Request const& req) const noexcept
 {
-    setenv("HTTP:METHOD", "POST", 1);
-    setenv("HTTP:PATH", req.get_path().c_str(), 1);
+    int err;
+    err = setenv("HTTP:METHOD", "POST", 1);
+    err = setenv("HTTP:PATH", req.get_path().c_str(), 1);
     for (auto const& element : req.get_query()) 
-    	setenv(("HTTP:QUERY:" + element.first).c_str(), element.second.c_str(), 1); 
+    	err = setenv(("HTTP:QUERY:" + element.first).c_str(), element.second.c_str(), 1); 
     for (auto const& element : req.get_headers()) 
-    	setenv(("HTTP:HEADER:" + element.first).c_str(), element.second.c_str(), 1); 
+    	err = setenv(("HTTP:HEADER:" + element.first).c_str(), element.second.c_str(), 1); 
     for (auto const& element : req.get_body()) 
-    	setenv(("HTTP:BODY:" + element.first).c_str(), element.second.c_str(), 1); 
+    	err = setenv(("HTTP:BODY:" + element.first).c_str(), element.second.c_str(), 1); 
+    return (err == -1)? false : true;
 }
