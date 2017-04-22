f219e602cc22b7e68af2319a7ee792df050daab6
Steven Wilson Kidd <kidd9@data.cs.purdue.edu>
2017-04-21 23:30:11 -0400

    Commit

--- src/controller/ExecScriptController.cpp
+++ src/controller/ExecScriptController.cpp
@@ -70,7 +70,7 @@ void ExecScriptController::run(Request const& req, Response& res) const
     // remove the trailing newline before returning
     std::string content_type(buf);
     content_type = content_type.substr(0, content_type.size() - 1);
-    res.send(content_type, content_type.length());
+    res.send(content_type, (int)content_type.length());
 }
 
 bool ExecScriptController::set_environment(Request const& req) const noexcept
