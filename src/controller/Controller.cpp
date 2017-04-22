7874f2e656c848e325de26ac83dcff6beddf45eb
Steven Wilson Kidd <kidd9@data.cs.purdue.edu>
2017-04-21 20:11:24 -0400

    Commit

--- src/controller/Controller.cpp
+++ src/controller/Controller.cpp
@@ -67,7 +67,7 @@ bool Controller::resolve_requested_path(std::string const& requested, std::strin
 	std::string request = real_path(basedir + requested);
 	std::string com_path = real_path(basedir) + requested;
 	com_path = real_path(com_path);
-	if (!request.find("/static/"))
+	if (request.find("/static/") == std::string::npos)
 		return false;
 	bool ret = false;
 	if (!com_path.compare(request)) {
